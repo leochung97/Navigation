483b3e81458530ea39a1768e106403bd2b4258a4
Leo Chung <leo.chung97@gmail.com>
2024-01-04 12:48:26 -0500

    Add NavigationNotes file

---
+++ Navigation/NavigationNotes.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationNotes.swift
+//  Navigation
+//
+//  Created by Leo Chung on 1/4/24.
+//
+
+import SwiftUI
+
+struct NavigationNotes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationNotes()
+}
