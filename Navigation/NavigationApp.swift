973ff75a8b54ed62675600b452307acc49d29f94
Leo Chung <leo.chung97@gmail.com>
2024-01-04 12:47:09 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Leo Chung on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
