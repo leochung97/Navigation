a0ef26afd54fd278ed4a82f0edd20381585e6173
Leo Chung <leo.chung97@gmail.com>
2024-01-04 17:07:43 -0500

    Notes on NavigationPath bindign

--- Navigation/NavigationNotes.swift
+++ Navigation/NavigationNotes.swift
@@ -24,6 +24,32 @@ struct Student: Hashable {
 
 // Programmatic navigation allows you to move from one view to another just using code rather than waiting for the user to take a specific action
 // This is done by binding the path of a NavigationStack to an array of whatever data you're navigating with ( NavigationStack(path: $path) )
+// You can also bind paths using NavigationPath, which is able to hold a variety of data types in a single path
+// NavigationPath is what is called a type-eraser -> it stores any kind of Hashable data without exposing exactly what type of data each item is
+
+struct RandomView: View {
+    @Binding var path: [Int]
+    // @Binding var path: NavigationPath
+    var number: Int
+    
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path.removeAll()
+                    // path = NavigationPath()
+                }
+            }
+    }
+}
+
+// If you're several views deep and want to return home:
+// 1. If using a simple array for your path, you can call removeAll() on the path and remove everything to go back to the root view
+// 2. If you are using NavigationPath, you can set it to a new, empty instance of NavigationPath like so: path = NavigationPath()
+// To return home from the subview, you can do two options:
+// 1. Store your path using an external class that uses an @Observable
+// 2. Use a new property wrapper called @Binding
 
 struct DetailView: View {
     var number: Int
@@ -39,26 +65,52 @@ struct DetailView: View {
 }
 
 struct NavigationNotes: View {
+    @State private var randomPath = [Int]()
+//    @State private var path = NavigationPath()
     @State private var path = [Int]()
     
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+        NavigationStack(path: $path) {
+            RandomView(path: $path, number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    RandomView(path: $path, number: i)
                 }
-
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
         }
+
+        
+//        NavigationStack(path: $path) {
+//            RandomView(number: 0)
+//                .navigationDestination(for: Int.self) { i in
+//                    RandomView(number: i)
+//                }
+//        }
+        
+//        NavigationStack(path: $path) {
+//            List {
+//                ForEach(0..<5) { i in
+//                    NavigationLink("Select Number: \(i)", value: i)
+//                }
+//
+//                ForEach(0..<5) { i in
+//                    NavigationLink("Select String: \(i)", value: String(i))
+//                }
+//            }
+//            .navigationDestination(for: Int.self) { selection in
+//                Text("You selected the number \(selection)")
+//            }
+//            .navigationDestination(for: String.self) { selection in
+//                Text("You selected the string \(selection)")
+//            }
+//            .toolbar {
+//                Button("Push 556") {
+//                    path.append(556)
+//                }
+//
+//                Button("Push Hello") {
+//                    path.append("Hello")
+//                }
+//            }
+//        }
         
 //        NavigationStack(path: $path) {
 //            VStack {
